5c05eb214b04256eee756d533ae113e90a3c625e
vikrant246 <161357735+vikrant246@users.noreply.github.com>
2024-05-12 14:43:54 +0530

    Created infra.tf

---
+++ scripts/infra.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "ap-south-1"
+  shared_config_files = ["/root/.aws/credentials"]
+}
+
+resource "aws_instance" "deploy" {
+  ami           = "ami-05e00961530ae1b55"
+  instance_type = "t2.medium"
+  key_name      = "medicure"
+
+  # Use the default security group
+  security_groups = ["default"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./medicure.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "my_kube_server"
+    }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+  provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/health_care/scripts/health-playbook.yml "
+  } 
+}
